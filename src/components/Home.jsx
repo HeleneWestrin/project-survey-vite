e602c2079c5bb71141358c56ebe69dd140046cd7
JoyceKuode <joyce.kuo@me.com>
2024-10-16 14:49:36 +0200

    Add image, title, and paragraph to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import { Button } from "./ui/Button";
+import homeImage from '../assets/home.png';
 
 export const Home = ({ setSurveyStarted }) => {
   return (
     <main>
-      I am the home screen.
+      <img src={homeImage} alt="Cartoon of a person relaxed and sleeping on the sofa with abstract shapes in the background" />
+      <h1>The Science of Happiness</h1>
+      <p>Everyday Joy Booster Survey</p>
       <Button onClick={() => setSurveyStarted(true)} text="Let's begin!" />
     </main>
   );
